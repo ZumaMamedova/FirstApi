64309b23355c8ad12d7c4241478a94cf1e597b59
ZumaMamedova <zumrudam@code.edu.az>
2023-07-15 12:33:53 +0400

    addfluent

--- FirstApi/FirstApi/Dtos/Product/ProductCreateDto.cs
+++ FirstApi/FirstApi/Dtos/Product/ProductCreateDto.cs
@@ -1,9 +1,34 @@
-﻿namespace FirstApi.Dtos.Product
+﻿using FluentValidation;
+
+namespace FirstApi.Dtos.Product
 {
     public class ProductCreateDto
     {
         public string Name { get; set; }
         public double SalePrice { get; set; }
         public double CostPrice { get; set; }
+    }
+    public class ProductCreateDtoValidator:AbstractValidator<ProductCreateDto> 
+    { 
+        public ProductCreateDtoValidator() 
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty().WithMessage("bos qoyma")
+                .MaximumLength(20).WithMessage("20 den yuxari olmaz");
+            RuleFor(p => p.SalePrice)
+                .NotNull().WithMessage("bos ola bilmez")
+                .GreaterThanOrEqualTo(0).WithMessage("0 dan boyuk olmalidi");
+            RuleFor(p => p.CostPrice)
+                .NotNull().WithMessage("bos ola bilmez")
+                .GreaterThanOrEqualTo(0).WithMessage("0 dan boyuk olmalidi");
+            RuleFor(p => p).Custom((p, context) =>
+            {
+                if (p.CostPrice > p.SalePrice)
+                {
+                    context.AddFailure("CostPrice", "CostPrice boyuk ola bilmez");
+                }
+            });
+                
+        }  
     }
 }
--- FirstApi/FirstApi/Program.cs
+++ FirstApi/FirstApi/Program.cs
@@ -1,4 +1,6 @@
 using FirstApi.DAL;
+using FirstApi.Dtos.Product;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,7 +8,12 @@ var config=builder.Configuration;
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+ .AddFluentValidation(option =>
+ {
+     option.RegisterValidatorsFromAssemblyContaining<ProductCreateDtoValidator>();
+ });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
